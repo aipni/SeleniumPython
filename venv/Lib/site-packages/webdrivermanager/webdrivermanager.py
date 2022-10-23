04121e9e3338b78a0f61f73e9715a7cacd414b10
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 14:08:05 +0000

    Adding tarfile member sanitization to extractall()

--- venv/Lib/site-packages/webdrivermanager/webdrivermanager.py
+++ venv/Lib/site-packages/webdrivermanager/webdrivermanager.py
@@ -280,7 +280,26 @@ class WebDriverManagerBase:
             LOGGER.debug('Created directory: %s', extract_dir)
         if filename.lower().endswith('.tar.gz'):
             with tarfile.open(os.path.join(dl_path, filename), mode='r:*') as tar:
-                tar.extractall(extract_dir)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(tar, extract_dir)
                 LOGGER.debug('Extracted files: %s', ', '.join(tar.getnames()))
         elif filename.lower().endswith('.zip'):
             with zipfile.ZipFile(os.path.join(dl_path, filename), mode='r') as driver_zipfile:
